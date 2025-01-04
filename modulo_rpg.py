b19cfdf0858bc30350428fe1ae5f9f38eb8735f0
luis-barbara <luis.carlos.barbara@gmail.com>
2025-01-04 16:15:41 +0000

    build: ficheiros main.py e modulo_rpg.py adicionados

---
+++ modulo_rpg.py
@@ -0,0 +1,7 @@
+''' Jogo de RPG Textual: Contexto - Criar um jogo de RPG textual onde o utilizador pode
+criar uma personagem, explorar um mundo fictício e tomar decisões que afetam o
+enredo. Nota: O aluno deve criar estruturas para representar o personagem e o
+mundo do jogo. '''
+
+
+# Desenvolvimento
